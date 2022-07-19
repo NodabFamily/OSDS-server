c0c8bd53bc7e822a61221a563a391626f6446c1b
DaheePark <ekgml3219@naver.com>
2022-07-20 02:25:52 +0900

    feat: Add album, base_model, comment, like, photo, tag models

---
+++ archives/models/album.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from archives.models.base_model import BaseModel
+from accounts.models.family import Family
+
+class Album(BaseModel):
+    family_id = models.ForeignKey(Family, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=31)
+    cover_image = models.ImageField(upload_to="album/", null=True, blank=True)
