2d9dedc50d18498bd25a82e2c7261e50345dd0b7
DaheePark <ekgml3219@naver.com>
2022-07-27 04:27:38 +0900

    feat: Add create_read_all_album view

--- archives/urls.py
+++ archives/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import views
-from .views import create_album
+from .views import create_read_all_album
 
 app_name = "archives"
 
 
 urlpatterns = [
-    path("families/<int:family_id>/album", create_album, name="create_album"),
+    path("families/<int:family_id>/album", create_read_all_album, name="create_read_all_album"),
 ]
 
--- archives/views.py
+++ archives/views.py
@@ -3,15 +3,17 @@ import json
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404
 from accounts.models import Family
-from .models import Album
+from .models import Album, Like, Photo
+
+from django.views.decorators.http import require_http_methods
 # Create your views here.
 
-
-def create_album(request, family_id):
+@require_http_methods(['POST', 'GET'])
+def create_read_all_album(request, family_id):
     if request.method == "POST":
 
         body = request.POST
-        img = request.FILES['cover_image']
+        img = request.FILES['album_image']
 
         new_album = Album.objects.create(
             family_id=get_object_or_404(Family, pk=family_id),
@@ -23,7 +25,7 @@ def create_album(request, family_id):
             "id": new_album.id,
             "title": new_album.title,
             "family_id": new_album.family_id.id,
-            "cover_image": new_album.cover_image.url,
+            "cover_image": new_album.album_image.url,
             "created_at": new_album.created_at.strftime("%m/%d/%Y, %H:%M:%S"),
             "updated_at": new_album.updated_at.strftime("%m/%d/%Y, %H:%M:%S"),
 
@@ -44,9 +46,50 @@ def create_album(request, family_id):
             status=200
         )
 
-    return JsonResponse({
-        'status': 405,
-        'success': False,
-        'message': 'method error',
-        'data': None
-    })
+    elif request.method == "GET":
+        # 변수 초기화
+        like_count = 0
+        comment_count = 0
+
+        user = request.user.family_id
+
+        album_all = Album.objects.filter(family_id=user)
+        album_json_all = []
+
+        for album in album_all:
+            photo_all = Photo.objects.filter(album_id=album.id)
+
+            for photo in photo_all:
+
+                like_count += photo.like_set.all().count()
+                comment_count += photo.comment_set.all().count()
+
+            album_json = {
+                "id" : album.id,
+                "family_id" : user,
+                "title" : album.title,
+                "album_image" : album.album_image.url,
+                "like_count" : like_count,
+                "comment_count" : comment_count,
+                "created_at" : album.created_at.strftime("%m/%d/%Y, %H:%M:%S"),
+                "updated_at" : album.updated_at.strftime("%m/%d/%Y, %H:%M:%S")
+            }
+            album_json_all.append(album_json)
+            like_count = 0
+            comment_count = 0
+
+        json_res = json.dumps(
+            {
+                "status": 200,
+                "success": True,
+                "message": "생성 성공!",
+                "data": album_json_all
+            },
+            ensure_ascii=False
+        )
+
+        return HttpResponse(
+            json_res,
+            content_type=u"application/json; charset=utf-8",
+            status=200
+        )
