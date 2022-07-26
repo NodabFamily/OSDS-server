ec205a169fb54c86a5492c6ac5c47da7bf6b43fb
DaheePark <ekgml3219@naver.com>
2022-07-27 00:35:41 +0900

    feat: Add create_album view

--- OSDS/urls.py
+++ OSDS/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('archives.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
