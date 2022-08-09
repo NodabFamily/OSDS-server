c490e16eea0fde80b515e87ac933aa0ea6e16c22
DaheePark <ekgml3219@naver.com>
2022-08-10 00:38:45 +0900

    refacter: Update album model, photo model

--- archives/models/album.py
+++ archives/models/album.py
@@ -6,5 +6,5 @@ from families.models import Family
 class Album(BaseModel):
     family_id = models.ForeignKey(Family, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=31)
-    album_image = models.ImageField(upload_to="album/", null=True, blank=True)
+    album_image = models.URLField()
 
