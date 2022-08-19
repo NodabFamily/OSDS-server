76566b1bd4c0d233174c2dace4031533e4478c62
DaheePark <ekgml3219@naver.com>
2022-08-19 16:34:38 +0900

    fix: User model CharField max_length

--- accounts/models/user.py
+++ accounts/models/user.py
@@ -5,10 +5,10 @@ from families.models import Family
 
 class User(AbstractUser):
     family_id = models.ForeignKey(Family, on_delete=models.CASCADE, null=True, blank=True)
-    member_id = models.CharField(max_length=31, null=True, default='')
-    password = models.CharField(max_length=31, null=True, default='')
-    name = models.CharField(max_length=31, null=True, default='')
-    birth = models.CharField(max_length=31, null=True, default='')
+    member_id = models.CharField(max_length=255, null=True, default='')
+    password = models.CharField(max_length=255, null=True, default='')
+    name = models.CharField(max_length=255, null=True, default='')
+    birth = models.CharField(max_length=255, null=True, default='')
     bio = models.CharField(max_length=255, null=True, default='')
     is_participant = models.BooleanField(null=False, default=False)
     avatar = models.ImageField(upload_to='user/', null=True, blank=True)
